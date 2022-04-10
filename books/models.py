fd97423f7268334c8fa9f16ff2427b497afcf835
elissabj <elissabjg@gmail.com>
2022-04-09 21:12:06 -0500

    changes

--- books/models.py
+++ books/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-# Create your models here.
+
 class Author(models.Model):
 	name = models.CharField(max_length=128)
 	last_name = models.CharField(max_length=128, null=True)
--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,11 @@
-from books import views
+from . import views
 from rest_framework import routers
 from django.urls import path, include
 
 router = routers.DefaultRouter()
-router.register(r'books', views.BookViewSet)
-router.register(r'groupsBooks', views.BookViewSet)
+router.register(r'', views.BookViewSet)
+router.register(r'author', views.AuthorViewSet)
+router.register(r'bookAuthor', views.BooksAuthorsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
