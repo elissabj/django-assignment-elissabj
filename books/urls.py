da74f1b845d6df6694ea9161983247c7a5cd5302
elissabj <elissabjg@gmail.com>
2022-04-11 15:43:07 +0000

    modifyBooksurls

--- books/urls.py
+++ books/urls.py
@@ -3,9 +3,10 @@ from rest_framework import routers
 from django.urls import path, include
 
 router = routers.DefaultRouter()
-router.register(r'books', views.BookViewSet)
+
 router.register(r'authors', views.AuthorViewSet)
 router.register(r'bookauthors', views.BooksAuthorsViewSet)
+router.register(r'', views.BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
